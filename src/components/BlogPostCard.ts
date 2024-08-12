92f2b2697cd1874b1f12c6f66f32fea48a852442
toyskip725 <toyskip725@gmail.com>
2024-08-13 00:18:11 +0900

    [feature] create BLogPostCard component

---
+++ src/components/BlogPostCard.ts
@@ -0,0 +1,48 @@
+import {LitElement, css, html} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+
+interface FrontMatter {
+  title: string;
+  publishedAt: Date;
+  tags: Array<string>;
+}
+
+@customElement('blog-post-card')
+export class BlogPostCard extends LitElement {
+  static styles = css`
+    .container {
+      border-bottom: solid 1px #2a2a31;
+      margin: 1em 0em;
+    }
+    .title {
+      color: #2a2a31;
+      font-size: large;
+      margin-top: 0em;
+    }
+    .title a {
+      color: #2a2a31;
+      text-decoration: none;
+    }
+    .published-at {
+      color: #2a2a31;
+      font-size: small;
+      margin-bottom: 0em;
+    }
+  `;
+
+  @property() href: string = "";
+  @property() frontmatter?: FrontMatter = undefined;
+
+  render() {
+    if (this.frontmatter === undefined) return html`<div></div>`;
+
+    return html`
+      <div class="container">
+        <p class="published-at">${this.frontmatter.publishedAt.toDateString()}</p>
+        <p class="title">
+          <a href=${this.href}>${this.frontmatter.title}</a>
+        </p>
+      </div>
+    `;
+  }
+}
